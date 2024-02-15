869083dea1774cdcb0d92d21763d5e49f2955d3c
tuanGLX <tuanp@galaxystudio.vn>
2024-02-16 01:03:01 +0700

    first init

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+require("dotenv").config();
+
+import configViewEngine from "./config/viewEngine";
+import initWebRoutes from "./routes/api";
+
+const app = express();
+
+configViewEngine(app);
+
+initWebRoutes(app);
+
+
+const PORT = process.env.PORT || 6969;
+app.listen(PORT, () => {
+  console.log("Server is running on port:",PORT);
+});
