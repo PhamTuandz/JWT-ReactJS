2acad4af61230c7db4a82e77ac9686261e5dbbb7
tuanGLX <tuanp@galaxystudio.vn>
2024-02-20 22:27:23 +0700

    install sequelize

--- src/index.js
+++ src/index.js
@@ -4,18 +4,22 @@ require("dotenv").config();
 import configViewEngine from "./config/viewEngine";
 import initWebRoutes from './routes/web';
 import bodyParser from "body-parser";
+import checkConnection from "./config/connectDB";
 
+checkConnection()
 const app = express();
 
 configViewEngine(app);
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 initWebRoutes(app);
 
 
 const PORT = process.env.PORT || 6969;
 app.listen(PORT, () => {
-  console.log("Server is running on port:",PORT);
+  console.log("Server is running on port:", PORT);
 });
