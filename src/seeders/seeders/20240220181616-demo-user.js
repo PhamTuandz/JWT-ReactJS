950cd37d450df4e2b9876634564e6aa6c752a83f
tuanGLX <tuanp@galaxystudio.vn>
2024-02-21 01:40:08 +0700

    update stucture source

---
+++ src/seeders/seeders/20240220181616-demo-user.js
@@ -0,0 +1,29 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+   
+    */
+    await queryInterface.bulkInsert('Users', [{
+      email: 'random@gmail.com',
+      username: 'John Doe',
+      password: '123456',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }], {});
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
